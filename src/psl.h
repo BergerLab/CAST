74b11f48252333241d55d91557b28e123d72aa26
AndyHaskell <xcrnr2013@gmail.com>
2014-07-24 16:29:52 -0400

    Added the psl struct

---
+++ src/psl.h
@@ -0,0 +1,30 @@
+#ifndef __CABLAT_PSL_H__
+#define __CABLAT_PSL_H__
+
+#include <stdlib.h>
+
+struct psl_entry {
+    int matches;
+    int mismatches;
+    int rep_matches;
+    int n_count;
+    int q_num_insert;
+    int q_base_insert;
+    int t_num_insert;
+    int t_base_insert;
+    char *strand;
+    char *q_name;
+    unsigned q_size;
+    int q_start;
+    int q_end;
+    char *t_name;
+    unsigned t_size;
+    int t_start;
+    int t_end;
+    unsigned block_count;
+    unsigned block_size;
+    unsigned q_starts;
+    unsigned t_starts;
+};
+
+#endif
